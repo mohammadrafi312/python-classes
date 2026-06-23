2fe260abe955177413ec985ff067d3a269fa4684
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-06-23 13:18:51 +0530

    class.py modified and inheritance added

--- class.py
+++ class.py
@@ -1,6 +1,14 @@
-class dog:#class
-    def __init__(self,fullname):#constructor
-        self.name=fullname
-        print(fullname)#message
+# class dog:#class
+#     def __init__(self,fullname):#constructor
+#         self.name=fullname
+#         print(fullname)#message
 
-d1=dog("Bud")#object
+# d1=dog("Bud")#object
+
+class student :
+    college_name="ABC College"#class variable
+    def __init__(self,name,marks):#constructor
+        self.name=name#instance variable
+        self.marks=marks#instance variable  
+    def average(self):#instance method
+        return self.marks/3
---
+++ inheritance.py
@@ -0,0 +1,76 @@
+# single inheritance is a feature of object-oriented programming where a class (called a child or subclass) can inherit attributes and methods from another class (called a parent or superclass). This allows for code reusability and the creation of a hierarchical relationship between classes.
+
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+class dog(Animal):
+    pass
+    # def sound(self):
+    #     print("bark")
+d1=dog()
+d1.sound()
+
+
+#multiple inheritance is a feature of object-oriented programming where a class can inherit
+class Father:
+
+    def skills(self):
+        print("Driving")
+
+class Mother:
+
+    def talent(self):
+        print("Cooking")
+
+class Child(Father, Mother):
+    pass
+
+c = Child()
+
+c.skills()
+c.talent()
+
+
+#multi level inheritance is a feature of object-oriented programming where a class can inherit
+class Animal:
+
+    def sound(self):
+        print("Animal Sound")
+
+class Dog(Animal):
+
+    def bark(self):
+        print("Bark")
+
+class Puppy(Dog):
+    pass
+
+p = Puppy()
+
+p.sound()
+p.bark()
+# hybrid inheritance is a feature of object-oriented programming where a class can inherit
+
+# Base class
+class Parent:
+    def func1(self):
+        print("This function is in parent class.")
+
+# Derived class 1
+class Child1(Parent):
+    def func2(self):
+        print("This function is in child 1.")
+
+# Derived class 2
+class Child2(Parent):
+    def func3(self):
+        print("This function is in child 2.")
+
+# Driver code
+object1 = Child1()
+object2 = Child2()
+
+object1.func1()
+object1.func2()
+object2.func1()
+object2.func3()
