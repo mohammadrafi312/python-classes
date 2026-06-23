a02a908e18fdd2b92861a5896e5f5034e3b6f0c3
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-06-23 14:33:51 +0530

    new file is created named oops

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -49,7 +49,7 @@ p = Puppy()
 
 p.sound()
 p.bark()
-# hybrid inheritance is a feature of object-oriented programming where a class can inherit
+# hierarchical inheritance is a feature of object-oriented programming where a class can inherit
 
 # Base class
 class Parent:
@@ -74,3 +74,29 @@ object1.func1()
 object1.func2()
 object2.func1()
 object2.func3()
+
+#hybrid inheritance is a feature of object-oriented programming where a class can inherit
+# Base class
+class School:
+    def func1(self):
+        print("This function is in school.")
+
+# Derived class 1 (Single Inheritance)
+class Student1(School):
+    def func2(self):
+        print("This function is in student 1.")
+
+# Derived class 2 (Another Single Inheritance)
+class Student2(School):
+    def func3(self):
+        print("This function is in student 2.")
+
+# Derived class 3 (Multiple Inheritance)
+class Student3(Student1, School):
+    def func4(self):
+        print("This function is in student 3.")
+
+# Driver code
+obj = Student3()
+obj.func1()
+obj.func2()
